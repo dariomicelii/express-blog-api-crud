5501ae8ea6dabf6dd805e81869f925f5a865ef37
dariomicelii <dariomiceli01@gmail.com>
2024-11-22 16:09:14 +0100

    app.js attuale

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 
+// importiamo il router
 const postsRouter = require("./routers/posts.js");
+
+// body parser
+app.use(express.json());
 
 app.use("/posts", postsRouter);
 
