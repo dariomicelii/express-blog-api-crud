4599c51e6dd104da773932f4dd0e285bee87ae32
dariomicelii <dariomiceli01@gmail.com>
2024-11-25 16:24:32 +0100

    app.js da ordinare

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+const errorsHandler = require("./middlewares/errorsHandler.js");
+const notFound = require("./middlewares/notFound.js");
+
 app.use(express.static("public"));
 
 // importiamo il router
@@ -8,9 +12,13 @@ const postsRouter = require("./routers/posts.js");
 
 // body parser
 app.use(express.json());
-
 app.use("/posts", postsRouter);
+
+app.use(errorsHandler);
+app.use(notFound);
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
 });
+
+//! RICORDARSI DI CONTROLLARE L'ORDINE DELLE CONFIGUARAZIONI
