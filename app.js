4b5403ffd820f7de022789a55894d6416a9d9352
dariomicelii <dariomiceli01@gmail.com>
2024-11-25 16:30:25 +0100

    app.js ordinato

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// REGISTERING HANDLERS
 const errorsHandler = require("./middlewares/errorsHandler.js");
 const notFound = require("./middlewares/notFound.js");
 
 app.use(express.static("public"));
-
-// importiamo il router
-const postsRouter = require("./routers/posts.js");
-
-// body parser
 app.use(express.json());
+
+// REGISTERING ROUTES
+const postsRouter = require("./routers/posts.js");
 app.use("/posts", postsRouter);
 
+// ERROR HANDLERS
 app.use(errorsHandler);
 app.use(notFound);
 
+// START LISTENING
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
 });
