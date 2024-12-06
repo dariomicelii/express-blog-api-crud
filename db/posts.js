0d80c1a63679fb81b7affcbc65003d22b64fa5f2
dariomicelii <dariomiceli01@gmail.com>
2024-12-06 10:57:11 +0100

    post cambiato per fetch

--- db/posts.js
+++ db/posts.js
@@ -1,42 +1,42 @@
 let posts = [
   {
     id: 1,
-    titolo: "Ciambellone",
-    contenuto: "Ricetta ciambellone",
-    immagine: "/images/ciambellone.jpg",
-    tag: ["#ciambellone", "#cucina", "#dolce"],
+    title: "Ozzy Osbourne",
+    content: "Ozzy Osbourne è un cantante britannico",
+    image: "/images/ciambellone.jpg",
+    category: "Musica",
   },
 
   {
     id: 2,
-    titolo: "Cracker alla barbabietola",
-    contenuto: "Ricetta cracker alla barbabietola",
-    immagine: "/images/cracker_barbabietola.jpg",
-    tag: ["#crackerbarbabietola", "#cucina", "#verdure"],
+    title: "Elton John",
+    content: "Elton John è un cantante e pianista",
+    image: "/images/cracker_barbabietola.jpg",
+    category: "Musica",
   },
 
   {
     id: 3,
-    titolo: "Pane fritto dolce",
-    contenuto: "Ricetta Pane fritto dolce",
-    immagine: "/images/pane_fritto_dolce.jpg",
-    tag: ["#Panefrittodolce", "#cucina", "#dolce"],
+    title: "Jim Morrison",
+    content: "Jim Morrison è stato un cantante frontman della band 'The Doors'",
+    image: "/images/pane_fritto_dolce.jpg",
+    category: "Musica",
   },
 
   {
     id: 4,
-    titolo: "Pasta alla barbabietola",
-    contenuto: "Ricetta Pasta alla barbabietola",
-    immagine: "/images/pasta_barbabietola.jpg",
-    tag: ["#Pastaallabarbabietola", "#cucina", "#pasta", "#verdure"],
+    title: "The Zen Circus",
+    content: "Gli Zen Circus sono una band Pisana",
+    image: "/images/pasta_barbabietola.jpg",
+    category: "Musica",
   },
 
   {
     id: 5,
-    titolo: "Torta paesana",
-    contenuto: "Ricetta torta paesana",
-    immagine: "/images/torta_paesana.jpg",
-    tag: ["#Pastaallabarbabietola", "#cucina", "#pasta", "#verdure"],
+    title: "Elvis Presley",
+    content: "Elvis Presley è stato il più grande di tutti i tempi",
+    image: "/images/torta_paesana.jpg",
+    category: "Musica",
   },
 ];
 
