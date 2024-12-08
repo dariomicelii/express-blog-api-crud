3cda49119cb6622273793551edf08bc0e5d9b4a5
dariomicelii <dariomiceli01@gmail.com>
2024-12-08 10:58:47 +0100

    posts (db)

--- db/posts.js
+++ db/posts.js
@@ -3,7 +3,8 @@ let posts = [
     id: 1,
     title: "Ozzy Osbourne",
     content: "Ozzy Osbourne è un cantante britannico",
-    image: "/images/ciambellone.jpg",
+    image:
+      "https://tse2.mm.bing.net/th?id=OIP.6Czmop99lGEkV90wWQ2V8wHaFO&pid=Api&P=0&h=180",
     category: "Musica",
   },
 
@@ -11,7 +12,8 @@ let posts = [
     id: 2,
     title: "Elton John",
     content: "Elton John è un cantante e pianista",
-    image: "/images/cracker_barbabietola.jpg",
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghQGnDbTy7Vmh9h_7Sf5M-nAwrmRFghT9TlxZ3xoTgQaBGDIYKzz9TgnsOtDFynIq9d5zWz3HIx-5BMpJEFRKHugNfG780uZhJTZUIo_fpUYHySDxSVzqv4Q68P7fwGpQqdKdWrjpoIe2X8h-2ZUyJgi2sYtxVTU4Dxp29kbnzRSeVTDI23CSl9ieBUw/s1600/elton-john-protagoniza-un-anuncio-que-te-hara-soltar-una-lagrimita-1542393128.jpg",
     category: "Musica",
   },
 
@@ -19,7 +21,8 @@ let posts = [
     id: 3,
     title: "Jim Morrison",
     content: "Jim Morrison è stato un cantante frontman della band 'The Doors'",
-    image: "/images/pane_fritto_dolce.jpg",
+    image:
+      "https://tse3.mm.bing.net/th?id=OIP.c0LSQ5Xqou2pqysF3FNtSwHaE5&pid=Api&P=0&h=180",
     category: "Musica",
   },
 
@@ -27,7 +30,8 @@ let posts = [
     id: 4,
     title: "The Zen Circus",
     content: "Gli Zen Circus sono una band Pisana",
-    image: "/images/pasta_barbabietola.jpg",
+    image:
+      "https://tse3.mm.bing.net/th?id=OIP.E_H2aq7v8w25S-Td6O17GgHaE8&pid=Api&P=0&h=180",
     category: "Musica",
   },
 
@@ -35,7 +39,8 @@ let posts = [
     id: 5,
     title: "Elvis Presley",
     content: "Elvis Presley è stato il più grande di tutti i tempi",
-    image: "/images/torta_paesana.jpg",
+    image:
+      "https://www.billboard.com/wp-content/uploads/2022/06/elvis-presley-portrait-1957-billboard-pro-1260.jpg",
     category: "Musica",
   },
 ];
